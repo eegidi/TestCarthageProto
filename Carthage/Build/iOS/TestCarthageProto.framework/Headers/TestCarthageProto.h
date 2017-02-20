dfc7f55a4a257f639ffb5a44e787094caadd7e9a
duel <duel@duels-Mac-mini.local>
2017-02-20 16:50:04 +0100

    added protocol buffer framework

---
+++ Carthage/Build/iOS/TestCarthageProto.framework/Headers/TestCarthageProto.h
@@ -0,0 +1,19 @@
+//
+//  TestCarthageProto.h
+//  TestCarthageProto
+//
+//  Created by duel on 17/02/17.
+//  Copyright © 2017 duel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TestCarthageProto.
+FOUNDATION_EXPORT double TestCarthageProtoVersionNumber;
+
+//! Project version string for TestCarthageProto.
+FOUNDATION_EXPORT const unsigned char TestCarthageProtoVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TestCarthageProto/PublicHeader.h>
+
+
